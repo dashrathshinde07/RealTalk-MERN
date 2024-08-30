b4e8a1127cbe9b2874ca8e793b036829baf78de8
dashrathshinde07 <dashrathbshinde@gmail.com>
2024-08-30 11:46:06 +0530

    feat: Added styling for Home, Navigation, Card, and Button components
    - Implemented CSS modules for the `Home`, `Navigation`, `Card`, and `Button` components.
    - Styled the `Home` component for a welcoming user interface, including custom link styles.
    - Enhanced the `Navigation` component with brand styling and layout adjustments.
    - Applied consistent and responsive styles to the `Card` and `Button` components.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,16 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
+import Navigation from "./components/shared/Navigation/Navigation";
+import Register from "./pages/Register/Register";
 
 function App() {
   return (
     <BrowserRouter>
+      <Navigation />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </BrowserRouter>
   );
